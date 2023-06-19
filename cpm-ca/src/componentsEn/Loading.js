ef5df87ecbbaedc589a82a3ed6aa2df8a3054021
crazy-net-hacker <mysterious.dev2020@gmail.com>
2023-06-19 13:28:57 +0000

    add

--- cpm-ca/src/componentsEn/Loading.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-import LoadingIcon from '../images/loading-icon.gif'
-
-export default ({size}) => <div style={{maxWidth: size}}>
-    <img src={LoadingIcon} alt="Loading Icon" style={{ width:'100%'}} />
-</div>
